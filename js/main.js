8f3926f796446588bca8f8d0b274734efcc0b11e
zbourazz <bourazzazakariae@gmail.com>
2025-07-31 12:08:01 +0100

    wregenerate

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let tet =document.getElementsByClassName('tetris-header')
+let expected  =document.getElementsByClassName('tetris-predected')
+
+for(let i=0;i<200;i++){
+    let dive =document.createElement('div')
+    dive.id=i
+    tet[0].appendChild(dive)
+    
+
+}
+for(let i=0;i<50;i++){
+    let dive =document.createElement('div')
+    dive.id=i
+    expected[0].appendChild(dive)
+    
+
+}
